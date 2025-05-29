1818c3416a7e48a7ecb9ee3ba365c4fafde71bdd
fandhysh <fandhy890@gmail.com>
2025-05-29 20:54:44 +0700

    perbaikan feedback

--- app/services/feedback_service.py
+++ app/services/feedback_service.py
@@ -1,11 +1,16 @@
 from app import db  # import PyMongo instance dari app mu
 
 
-def simpan_feedback(email: str, date: str, pain_level: int):
+def simpan_feedback(email: str, date: str, daftarGerakan: str, pain_level: int):
     """
     Simpan satu dokumen feedback pain scale ke koleksi `feedback`.
     """
-    doc = {"email": email, "date": date, "pain_level": pain_level}
+    doc = {
+        "email": email,
+        "date": date,
+        "daftar_gerakan": daftarGerakan,
+        "pain_level": pain_level,
+    }
     result = db.db.feedback.insert_one(doc)
     return result
 
