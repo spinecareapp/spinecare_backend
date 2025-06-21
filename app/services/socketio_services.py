309e4f91bc8c289064d6a6a4b67883f8f06ec99f
fandhysh <fandhy890@gmail.com>
2025-06-21 18:36:51 +0700

    warna pipe

--- app/services/socketio_services.py
+++ app/services/socketio_services.py
@@ -64,8 +64,12 @@ def handle_image(data):
                 image,
                 results.pose_landmarks,
                 mp_holistic.POSE_CONNECTIONS,
-                mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=4),
-                mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2),
+                mp_drawing.DrawingSpec(
+                    color=(71, 130, 141), thickness=2, circle_radius=4
+                ),
+                mp_drawing.DrawingSpec(
+                    color=(255, 255, 255), thickness=5, circle_radius=2
+                ),
             )
 
             try:
