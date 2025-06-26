c6318dd544350cb4be4573234fdca6f953343c0a
fandhysh <fandhy890@gmail.com>
2025-06-26 10:34:16 +0700

    landing page

--- app/routes/root_routes.py
+++ app/routes/root_routes.py
@@ -1,17 +1,18 @@
-from flask import Blueprint, request, jsonify
-from app.services.root_services import delete_account, hello_world, privacy_policy
+from flask import Blueprint, render_template, request, jsonify
+from app.services.root_services import delete_account, privacy_policy
 
 bp = Blueprint("dasar", __name__, url_prefix="/")
 
 
 @bp.route("/", methods=["GET"])
-def hello():
-    return hello_world()
+def landing_page():
+    return render_template("landing.html")
 
 
 @bp.route("/delete-account", methods=["GET"])
 def delete():
     return delete_account()
+
 
 @bp.route("/privacy-policy", methods=["GET"])
 def privacy():
